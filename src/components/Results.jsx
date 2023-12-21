cf02ab21bdcebcd0df5398fda379c3bffe97ec22
Yelena Strizhakova <e.strizhakova@gmail.com>
2023-12-21 15:28:06 +0400

    feat: user input and results table

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,24 +1,34 @@
-export default function Results(annualData) {
+import { calculateInvestmentResults, formatter } from '../util/investment';
+
+export default function Results({ input }) {
+  const resultsData = calculateInvestmentResults(input);
+
   return (
     <table id="result">
       <thead>
         <tr>
-          <td>Year</td>
-          <td>Investment Value</td>
-          <td>Interest (Year)</td>
-          <td>Total Interest</td>
-          <td>Invested Capital</td>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
         </tr>
       </thead>
       <tbody>
-          {/* {annualData.map((data, dataIndex) => (
-            <tr key={dataIndex}>
-              <td>{data.valueEndOfYear}</td>
-              <td>{data.interest}</td>
-              <td>{data.valueEndOfYear}</td>
-              <td>{data.annualInvestment}</td>
+        {resultsData.map((yearData) => {
+          const totalInterest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - input.initialInvestment;
+          const investedCapital = yearData.valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(investedCapital)}</td>
             </tr>
-          ))} */}
+          );
+        })}
       </tbody>
     </table>
   );
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,24 +1,48 @@
-export default function UserInput() {
+export default function UserInput({ userInput, onUserInputChange }) {
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          <input type="text" name="" id="" />
+          <input
+            required
+            type="number"
+            onChange={onUserInputChange}
+            name="initialInvestment"
+            value={userInput.initialInvestment}
+          />
         </p>
         <p>
           <label>Annual Investment</label>
-          <input type="text" name="" id="" />
+          <input
+            required
+            type="number"
+            onChange={onUserInputChange}
+            name="annualInvestment"
+            value={userInput.annualInvestment}
+          />
         </p>
       </div>
       <div className="input-group">
         <p>
           <label>Expected Return</label>
-          <input type="text" name="" id="" />
+          <input
+            required
+            type="number"
+            name="expectedReturn"
+            onChange={onUserInputChange}
+            value={userInput.expectedReturn}
+          />
         </p>
         <p>
           <label>Duration</label>
-          <input type="text" name="" id="" />
+          <input
+            required
+            type="number"
+            name="duration"
+            onChange={onUserInputChange}
+            value={userInput.duration}
+          />
         </p>
       </div>
     </section>
